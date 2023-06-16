23e03b775ccec2d3a79198724fedc30321c97607
Y01CMADRIAGA <Y01CMADRIAGA@NB-Y01CMADRIAGA.tecpetrol.techint.net>
2023-06-16 13:45:37 -0300

    se agrega modificaciones

--- Ordenadornum/src/Ordenadormum/Main.java
+++ Ordenadornum/src/Ordenadormum/Main.java
@@ -1,7 +1,10 @@
 package Ordenadormum;
+import java.util.Arrays;
 //import java.lang.reflect.Array;
 import java.util.Scanner;
-//import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 //import java.util.List;
 
 
@@ -23,11 +26,49 @@ public class Main {
 	private static void ordenar(String datonum1) {
 		// TODO Auto-generated method stub
 		// lo voy a pasar como lista
+		int n=0;
+		int m=0;
 		String separador =",";
 		String dato = datonum1;
+		// se va a crear un array dato1
 		String[] dato1 = dato.split(separador);
+		for(int i=0; i < datonum1.length(); i++) {
+			
+			
+			
+			if(datonum1.charAt(i) == ',') {
+				n = n+1;
+				m=n + 1;
+			}
+			
+		}
+		for(int i=0 ; i < m; i++) {
 		
-		System.out.println(dato1[0]);
+		//.............................................................................................
+			
+			
+			String[] separatedStrings = datonum1.split(",");
+		        int[] intArray = new int[m];
+		        
+		        for (int i1 = 0; i1 < m; i1++) {
+		            
+		            try {
+		                intArray[i1] = Integer.parseInt(separatedStrings[i1]);
+		            } catch (Exception e) {
+		                System.out.println("Unable to parse string to int: " + e.getMessage());
+		            }
+		        }
+		        
+		        System.out.println(Arrays.toString(intArray));
+			
+		//.............................................................................................	
+		
+		System.out.println(" se evalua los datos :" + dato1[i]);
+		
+		
+		}
+		
+		
 		//List<String> orden = Array.asList(daton.split("\\s*,\\s*"));
 	/*	for(String dato2:dato1) {
 			System.out.println(dato1[0]);
@@ -48,21 +89,8 @@ public class Main {
 		System.out.println(" los numeros ingresados son :  "+ datonum1);
 		return  datonum1;
 		
-		/*
-		for(int i=0; i<datonum1.length(); i++) {
-			
-			
-			
-			if(datonum1.charAt(i) == ' ' || datonum1.charAt(i) == ',') {
-				n = n+1;
-				
-			}
-			else {
-				
-			}
-			*/
+		
 		}
-	//	System.out.println(" la cantidad de blancos son :  "+n);
 	
 	
 	}
